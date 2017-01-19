f9c1e10f814211d2bd67eceeea0d52c559da49c6
Jean Palate <jean.palate@nbb.be>
2017-01-19 12:41:30 +0100

    RegArima classes. Test

--- R files/jd_class.R
+++ R files/jd_class.R
@@ -10,6 +10,7 @@ setClass("JD_Output", contains = "jobjRef")
 setClass("JD_SA", contains = "JD_Output")
 setClass("JD_TramoSeats", contains = "JD_SA")
 setClass("JD_X13", contains = "JD_SA")
+setClass("JD_RegArima", contains = "jobjRef")
 
 x13Process<-function(ts=s, method="RSA4c", details=NULL){
   jrslt<-sa_x13(s,method, details)
@@ -23,8 +24,8 @@ tramoseatsProcess<-function(ts=s, method="RSAfull", details=NULL){
   return(rslt)
 }
 
-
 setGeneric(name = "getTs", def = function(object, name, ...){standardGeneric("getTs")})
+setGeneric(name = "getRegArima", def = function(object){standardGeneric("getRegArima")})
 setMethod(f="getTs", signature = c(object="JD_Output", name="character"), definition = function(object, name, ...){proc_ts(object, name)})
 setMethod(f="print", signature="JD_SA",definition=function(x)
 {
@@ -38,7 +39,7 @@ setMethod(f="print", signature="JD_SA",definition=function(x)
 
 setMethod(f="print", signature="JD_X13",definition=function(x)
   {
-  cat("X13 (JD+)")
+  cat("X13 (JD+)\n")
   print(as(x, "JD_SA"))
 })
 
@@ -54,13 +55,31 @@ setMethod(f="show", signature="JD_X13",definition=function(object)
 })
 
 setMethod(f="show", signature="JD_TramoSeats",definition=function(object)
-  {
+{
   return( print("Tramo-Seats (JD+)"))
-  })
+})
+
+setMethod(f="getRegArima", signature="JD_TramoSeats",definition=function(object)
+{
+  return(new(Class = "JD_RegArima", jobj=object@jobj, jclass=object@jclass) )
+})
+
+setMethod(f="getRegArima", signature="JD_X13",definition=function(object)
+{
+  return(new(Class = "JD_RegArima", jobj=object@jobj, jclass=object@jclass) )
+})
+
 setMethod(f="plot", signature=c(x="JD_SA"), definition=function(x)
   {
   s<-getTs(x,"y")
   sa<-getTs(x,"sa")
   t<-getTs(x, "t")
   ts.plot(s,sa, t, col=c("black", "blue", "red"), main=show(x))
+})
+
+setMethod(f="Summary", signature="JD_RegArima", definition=function(x)
+{
+  cat("RegArima model\n")
+  print(proc_desc(x, "regression.description"))
+  print(proc_parameters(x, "regression.coefficients"))
 })
--- R files/jd_testclass.R
+++ R files/jd_testclass.R
@@ -6,12 +6,16 @@ s<-ts(data[,1], start=c(1995,1), frequency=12)
 
 rx13<-x13Process(s)
 plot(rx13)
-show(rx13)
 getTs(rx13, "y_f")
 
 rts<-tramoseatsProcess(s)
 plot(rts)
-show(rts)
 getTs(rts, "y_f")
 
 print(rts)
+
+show(rx13)
+Summary(getRegArima(rx13))
+
+show(rts)
+Summary(getRegArima(rts))
